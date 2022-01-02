2a2614ad82f4cdd1d08cd572d054fbffe7e0602d
rzinovyev <ruslan.zinovyev@hotmail.com>
2022-01-02 17:34:34 -0500

    scope lesson

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1984);
