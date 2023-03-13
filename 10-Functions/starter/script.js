4881922593b571c9511703a7e226c9e4690123d7
rzinovyev <ruslan.zinovyev@hotmail.com>
2023-03-12 22:16:29 -0400

    functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,58 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  //    ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 5, 200);
+
+// Primitive types vs Reference types
+const flight = 'LH234';
+const ruslan = {
+  name: 'Ruslan Zinovyev',
+  passport: 74820377493,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  //   if (passenger.passport === 74820377493) {
+  //     alert('Checki in');
+  //   } else {
+  //     alert('Wrond passport!');
+  //   }
+};
+
+checkIn(flight, ruslan);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
