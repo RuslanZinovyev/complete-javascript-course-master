06ef59141606443523621bc20e03947993c0c455
rzinovyev <ruslan.zinovyev@hotmail.com>
2023-04-29 22:42:11 -0400

    prototype details

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -4,11 +4,11 @@ const Person = function (firstName, birthYear) {
   // Instance propeties
   this.firstName = firstName;
   this.birthYear = birthYear;
+};
 
-  // NEVER DO THIS, INSTEAD WE HAVE TO USE PROTOTYPE
-  this.calcAge = function () {
-    console.log(2023 - this.birthYear);
-  };
+// Prototypes
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
 };
 
 const jonas = new Person('Jonas', 1991);
@@ -21,7 +21,7 @@ console.log(jonas);
 
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
-console.log(matilda.calcAge());
-console.log(jack.calcAge());
+matilda.calcAge();
+jack.calcAge();
 
 console.log(jack instanceof Person);
