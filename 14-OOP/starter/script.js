b1701a03297ac8ee9012f99f00a03cf5020bf2b8
rzinovyev <ruslan.zinovyev@hotmail.com>
2023-05-01 22:03:39 -0400

    fix

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -14,11 +14,6 @@ Person.prototype.calcAge = function () {
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
 
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 matilda.calcAge();
